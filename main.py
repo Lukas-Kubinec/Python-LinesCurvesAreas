ad5431361601fbf4b806308924375c5150f972a8
Lukas Kubinec <luko.kubinec@gmail.com>
2025-02-10 12:01:53 +0000

    added rotation, scale

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pygame
 import numpy as np
-
 pygame.init()
 
+# Window settings
 viewport = pygame.display.set_mode((640, 480))
 pygame.display.set_caption('Lines, curves and areas/blobs')
 viewport.fill((0, 0, 0))
 
+# FPS clock
+clock = pygame.time.Clock()
+
 # Line position
-line_a,line_b = [50,50] , [50,150]
+line_a,line_b = [150,150] , [200,200]
+
+# Star attributes
+
 
 # Functions
 # Move
@@ -19,21 +25,36 @@ def move_by(a,b,xy):
 
 # Scale
 def scale_by(a, b, scalar):
+    a_orig = a
+
+    a[0] = a[0] - b[0]
+    a[1] = a[1] - b[1]
+    b[0] = b[0] - a[0]
+    b[1] = b[1] - a[1]
+
     scalar = [[scalar[0],0],[0,scalar[1]]]
     scaled_a = [np.transpose(np.dot(scalar[0], np.transpose(a[0]))),np.transpose(np.dot(scalar[0], np.transpose(a[1])))]
     scaled_b = [np.transpose(np.dot(scalar[1], np.transpose(b[0]))),np.transpose(np.dot(scalar[1], np.transpose(b[1])))]
     scaled_a = np.transpose(scaled_a)
     scaled_b = np.transpose(scaled_b)
-    result = scaled_a + scaled_b
+    result = [scaled_a[0]+a_orig[0], scaled_a[1]+a_orig[1], scaled_b[0], scaled_b[1]]
     print("A: " + str(result[0]) + " B: " + str(result[1]))
     return result
 
 # Rotate
-def rotate_by(a,angle):
-    rads = angle*np.pi/180
+def rotate_by(a,b,angle):
+    rads = angle * np.pi / 180
+
+    a[0] = a[0] - b[0]
+    a[1] = a[1] - b[1]
+
     new_x = a[0] * np.cos(rads) - a[1] * np.sin(rads)
     new_y = a[0] * np.sin(rads) + a[1] * np.cos(rads)
-    return [new_x,new_y]
+
+    result = [new_x+b[0],new_y+b[1]]
+
+    print(result)
+    return result
 
 while True:
     for event in pygame.event.get():
@@ -49,12 +70,16 @@ while True:
             line_scalar = scale_by(line_a, line_b, [1.1, 1.1])
             line_a = line_scalar[0]
             line_b = line_scalar[1]
+            line_a[0],line_a[1] = int(line_a[0]),int(line_a[1])
+            line_b[0],line_b[1] = int(line_b[0]),int(line_b[1])
 
-        if pygame.mouse.get_pressed()[1]:
-            line_b = rotate_by(line_b,10)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 5:
+                line_b = rotate_by(line_b, line_a, 5)
 
-        if pygame.mouse.get_pressed()[2]:
-            line_b = rotate_by(line_b,-10)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 4:
+                line_b = rotate_by(line_b, line_a, -5)
 
         # Keyboard events
         if event.type == pygame.KEYDOWN:
@@ -85,3 +110,5 @@ while True:
 
     # UPDATE
     pygame.display.flip()
+    clock.tick(60)
+    pygame.display.set_caption('Lines, curves and areas/blobs | FPS:' + str(clock.get_fps()))
