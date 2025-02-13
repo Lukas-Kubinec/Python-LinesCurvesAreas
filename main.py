002eff5fe622329cde138ea70520dcaf6a9025b8
Lukas-Kubinec <luko.kubinec@gmail.com>
2025-02-13 15:59:12 +0000

    Shapes added

--- main.py
+++ main.py
@@ -4,15 +4,30 @@ import numpy as np
 pygame.init()
 
 # Window settings
-viewport = pygame.display.set_mode((640, 480))
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+viewport = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Lines, curves and areas/blobs')
 viewport.fill((0, 0, 0))
 
 # FPS clock
 clock = pygame.time.Clock()
 
+# SHAPES
+# Moving circle
+circle_diameter = 20
+circle_x, circle_y = 50, 50
+circle_hspeed = 3
+circle_vspeed = 3
+circle_color = (255, 0, 0)
+
 # Line position
 line_a,line_b = [150,150] , [200,200]
+
+# Triangle position
+triangle_a, triangle_b, triangle_c = [350,400] , [400,400] , [375,350]
+
+# Rectangle position
+rect_a, rect_b, rect_c, rect_d = [300,250] , [400,250] , [300,150] , [400,150]
 
 # Functions
 # Move
@@ -33,7 +48,6 @@ def scale_by(a, b, scalar):
     scaled_b = np.add(np.multiply(scalar, b), default_b)
     # Stores the new scaled vector positions, and returns them
     result = [[round(scaled_a[0], 2), round(scaled_a[1], 2)], [round(scaled_b[0], 2), round(scaled_b[1], 2)]]
-    print("A: " + str(result[0]) + " B: " + str(result[1]))
     return result
 
 # Rotate
@@ -49,37 +63,92 @@ def rotate_by(a,b,angle):
     result = [new_x+b[0],new_y+b[1]]
     return result
 
+# Random RGB
+def random_rgb():
+    return np.random.randint(0,255),np.random.randint(0,255),np.random.randint(0,255)
+
 # Used to determine if the program should be running
 program_running = True
 
 # Main loop
 while program_running:
-    for event in pygame.event.get():
-        # BEGIN
-        viewport.fill((0, 0, 0))
+    # BEGIN SECTION
+    viewport.fill((0, 0, 0))
 
-        # EVENTS
+    # EVENTS SECTION
+    # Movement of red circle
+    circle_y += circle_vspeed
+    if circle_y >= SCREEN_HEIGHT - circle_diameter:
+        circle_vspeed = -3
+        circle_color = random_rgb()
+    elif circle_y <= 0 + circle_diameter:
+        circle_vspeed = 3
+        circle_color = random_rgb()
+
+    circle_x += circle_hspeed
+    if circle_x >= SCREEN_WIDTH - circle_diameter:
+        circle_hspeed = -5
+        circle_color = random_rgb()
+    elif circle_x <= 0 + circle_diameter:
+        circle_hspeed = 5
+        circle_color = random_rgb()
+
+    # INPUT EVENTS SECTION
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             program_running = False
 
         # Mouse events
         if pygame.mouse.get_pressed()[0]:
+            # Scaling of line
             line_scalar = scale_by(line_a, line_b, [0.1,0.1])
             line_a = line_scalar[0]
             line_b = line_scalar[1]
+            # Scaling of triangle
+            triangle_c = scale_by(triangle_a, triangle_c, [0.1, 0.1])[1]
+            triangle_b = scale_by(triangle_a, triangle_b, [0.1, 0.1])[1]
+            # Scaling of rectangle
+            rect_b = scale_by(rect_a, rect_b, [0.1, 0.1])[1]
+            rect_c = scale_by(rect_a, rect_c, [0.1, 0.1])[1]
+            rect_d = scale_by(rect_a, rect_d, [0.1, 0.1])[1]
+
 
         if pygame.mouse.get_pressed()[2]:
+            # Scaling of line
             line_scalar = scale_by(line_a, line_b, [-0.1,-0.1])
             line_a = line_scalar[0]
             line_b = line_scalar[1]
+            # Scaling of triangle
+            triangle_c = scale_by(triangle_a, triangle_c, [-0.1, -0.1])[1]
+            triangle_b = scale_by(triangle_a, triangle_b, [-0.1, -0.1])[1]
+            # Scaling of rectangle
+            rect_b = scale_by(rect_a, rect_b, [-0.1, -0.1])[1]
+            rect_c = scale_by(rect_a, rect_c, [-0.1, -0.1])[1]
+            rect_d = scale_by(rect_a, rect_d, [-0.1, -0.1])[1]
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 5:
+                # Rotation of line
                 line_b = rotate_by(line_b, line_a, 5)
+                # Rotation of triangle
+                triangle_b = rotate_by(triangle_b, triangle_a, 5)
+                triangle_c = rotate_by(triangle_c, triangle_a, 5)
+                # Rotation of rectangle
+                rect_b = rotate_by(rect_b, rect_a, 5)
+                rect_c = rotate_by(rect_c, rect_a, 5)
+                rect_d = rotate_by(rect_d, rect_a, 5)
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 4:
+                # Rotation of line
                 line_b = rotate_by(line_b, line_a, -5)
+                # Rotation of triangle
+                triangle_b = rotate_by(triangle_b, triangle_a, -5)
+                triangle_c = rotate_by(triangle_c, triangle_a, -5)
+                # Rotation of rectangle
+                rect_b = rotate_by(rect_b, rect_a, -5)
+                rect_c = rotate_by(rect_c, rect_a, -5)
+                rect_d = rotate_by(rect_d, rect_a, -5)
 
         # Keyboard events
         if event.type == pygame.KEYDOWN:
@@ -104,13 +173,20 @@ while program_running:
                 line_a = [movement[0],movement[1]]
                 line_b = [movement[2],movement[3]]
 
-        # DRAW
-        pygame.draw.line(viewport, (255,255,255), line_a, line_b)
+    # DRAW SECTION
+    # Draw circle
+    pygame.draw.circle(viewport, circle_color, (circle_x, circle_y), circle_diameter)
+    # Draw line
+    pygame.draw.line(viewport, (255, 255, 255), line_a, line_b)
+    # Draw triangle
+    pygame.draw.polygon(viewport, (255, 255, 255), (triangle_a, triangle_b, triangle_c))
+    # Draw rectangle
+    pygame.draw.polygon(viewport, (255, 255, 255), (rect_a, rect_b, rect_d, rect_c))
 
     # UPDATE
     pygame.display.flip()
     clock.tick(60)
-    pygame.display.set_caption('Lines, curves and areas/blobs | FPS:' + str(round(clock.get_fps(),1)))
+    pygame.display.set_caption('Lines, curves and areas/blobs | FPS:' + str(int(clock.get_fps())))
 
 # Quits the game
 pygame.quit()
